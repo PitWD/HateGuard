a9f71aa1c4916d21cfbe77574faaff7c154428e3
PitWD <pit@lets.de>
2024-01-10 15:00:06 +0100

    PrintRainbow() added...

--- MEN.py
+++ MEN.py
@@ -23,3 +23,29 @@ def PrintMenuPos(key, text, keyColor=None, txtColor=None, offset=None):
     ESC.SetForeGround(txtColor)
     print(text, flush=True)
     ESC.ResetForeGround()
+
+
+def PrintRainbow(txt):
+    color =[31, 91, 33, 32, 36, 34, 95] # Solarized Red, Orange, Yellow, Green, Cyan, Blue, Violet
+    rest = 0
+    length = len(txt)
+    parts = 7
+
+    if length < parts:
+        parts = length
+    else:
+        rest = length % parts
+
+    chars_per_part = length // parts
+    for i in range(parts):
+        ESC.SetForeGround(color[i])
+        if i != 0 and rest != 0:
+            # No rest on 1st color
+            print(txt[:chars_per_part + 1], end="")
+            txt = txt[chars_per_part + 1:]
+            rest -= 1
+        else:
+            print(txt[:chars_per_part], end="")
+            txt = txt[chars_per_part:] 
+
+    ESC.ResetForeGround()
